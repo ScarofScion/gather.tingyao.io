e08e9bfa9945e547725357064fc7787081a0008b
scarofscion <scarofscion@gmail.com>
2018-12-21 14:19:00 -0800

    Updates

---
+++ precache-manifest.352537e643f2cd2faa7d6f723296045e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e321a114687a9c8ff59",
+    "url": "/gather.tingyao.io/static/js/runtime~main.9e321a11.js"
+  },
+  {
+    "revision": "d054d1e5a21fe5dc3e17",
+    "url": "/gather.tingyao.io/static/js/main.d054d1e5.chunk.js"
+  },
+  {
+    "revision": "bb0211114e50cf3dae2a",
+    "url": "/gather.tingyao.io/static/js/1.bb021111.chunk.js"
+  },
+  {
+    "revision": "d054d1e5a21fe5dc3e17",
+    "url": "/gather.tingyao.io/static/css/main.2913f7ef.chunk.css"
+  },
+  {
+    "revision": "63231dc9749635427a4bb2c856bdb9fa",
+    "url": "/gather.tingyao.io/index.html"
+  }
+];
